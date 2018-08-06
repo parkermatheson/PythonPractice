e4a0129d370304d6eddd101aa12475230281ea12
Parker <parkermatheson@gmail.com>
2018-08-06 14:51:34 -0600

    made logic for edge cases more explicit

--- HackerRank/is_leap.py
+++ HackerRank/is_leap.py
@@ -1,20 +1,23 @@
 def is_leap(year):
     leap = False
-    
+
     # A year is a leap year if:
-      # it's evenly divisible by 4 unless:
-        # the year can be evenly divided by 100, it is NOT a leap year, unless:
-          # the year is also evenly divisible by 400. Then it is a leap year.
+    # it's evenly divisible by 4 unless:
+    # the year can be evenly divided by 100, it is NOT a leap year, unless:
+    # the year is also evenly divisible by 400. Then it is a leap year.
     if year % 4 == 0:
-      if year % 100 != 0:
-        if year % 400 == 0:
-          leap = True
+        if year % 100 != 0:
+            if year % 400 == 0:
+                leap = True
+            else:
+                leap = False
+            leap = True
         else:
-          leap = False
-      else:
+            leap = False
+        leap = True
+    else:
         leap = False
-      leap = True
-    
+
     return leap
 
     year = int(input())
