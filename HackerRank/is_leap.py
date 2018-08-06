0df5051df1ea14c5650a179cb1ebdeaf436b7d96
Parker <parkermatheson@gmail.com>
2018-08-06 16:44:57 -0600

    changed logic from nested conditionals to an if:elif:else
    Changed logic from nested conditionals to an if:elif:else statement. This clarified the problem, and allowed me to see an error in my logic. The refactored code correctly evaluates additional edge cases.

--- HackerRank/is_leap.py
+++ HackerRank/is_leap.py
@@ -5,15 +5,9 @@ def is_leap(year):
     # it's evenly divisible by 4 unless:
     # the year can be evenly divided by 100, it is NOT a leap year, unless:
     # the year is also evenly divisible by 400. Then it is a leap year.
-    if year % 4 == 0:
-        if year % 100 != 0:
-            if year % 400 == 0:
-                leap = True
-            else:
-                leap = False
-            leap = True
-        else:
-            leap = False
+    if year % 4 == 0 and year % 100 != 0:
+        leap = True
+    elif year % 400 == 0:
         leap = True
     else:
         leap = False
