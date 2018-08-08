1d8f772e018978e2ddaa6c5bd9ffc48b19e15f83
Parker <parkermatheson@gmail.com>
2018-08-07 18:19:58 -0600

    Set the initial exercise requirements in the comments

---
+++ CodeExercises/anagrammer.py
@@ -0,0 +1,6 @@
+# --- Directions
+# Check to see if two provided strings are anagrams of each other.
+# --- Examples
+# anagrams('This Is A Bird!',  'British Dias') --> True
+# anagrams('Bloch duties',  'Duty Free') --> False
+
